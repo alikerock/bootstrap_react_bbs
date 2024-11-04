b9e4dbb55cd68777c65f53c5c37328e32da0129b
rock's ezweb <alikerock@gmail.com>
2024-11-04 10:36:04 +0900

    write.js import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import BoardList from './BoardList';
+import Write from './Write';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="container">
       <h1>React Board</h1>
       <BoardList/>
+      <Write/>
     </div>
   );
 }
--- src/Write.js
+++ src/Write.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default class Write extends Component {
   render() {
-    return (
+    return (      
       <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Group className="mb-3" controlId="title">
+          <Form.Label>제목</Form.Label>
+          <Form.Control type="text" name="title" placeholder="제목을 입력하세요" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+        <Form.Group className="mb-3" controlId="content">
+          <Form.Label>내용</Form.Label>
+          <Form.Control as="textarea" name="content" rows={3} />
         </Form.Group>
+        <div className="d-flex gap-1">
+          <Button variant="primary" type="submit">작성완료</Button>
+          <Button variant="secondary" type="reset">취소</Button>          
+        </div>      
       </Form>
     )
   }
